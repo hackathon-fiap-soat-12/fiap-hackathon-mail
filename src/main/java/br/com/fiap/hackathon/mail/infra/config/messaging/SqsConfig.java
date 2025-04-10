368754d09d6809582b013744c5db669d89ca1397
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-04-09 22:36:04 -0300

    ✨ feat: add dependency-check-maven-plugin

--- src/main/java/br/com/fiap/hackathon/mail/infra/config/messaging/SqsConfig.java
+++ src/main/java/br/com/fiap/hackathon/mail/infra/config/messaging/SqsConfig.java
@@ -2,20 +2,17 @@ package br.com.fiap.hackathon.mail.infra.config.messaging;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
-import java.net.URI;
-
 @Configuration
+@Profile("!local")
 public class SqsConfig {
 
 	@Bean
 	public SqsAsyncClient sqsAsyncClient() {
-		return SqsAsyncClient.builder()
-			.region(Region.US_EAST_1)
-			// .endpointOverride(URI.create("https://localhost.localstack.cloud:4566"))
-			.build();
+		return SqsAsyncClient.builder().region(Region.US_EAST_1).build();
 	}
 
 }
